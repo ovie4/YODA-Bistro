97cea784bcc7d4edbd2964faa71d34d03f796136
Ovie <you@example.com>
2017-12-21 14:03:51 -0500

    adds better post route

--- assets/js/server.js
+++ assets/js/server.js
@@ -11,23 +11,37 @@ app.use(bodyParser.json());
 
 //store new reservations in an array
 var reservations = [];
+var tables =[];
+var waitlist =[];
 
 
 //accept path requests and direct to relevant html files (routes)
 app.get("/", function(req,res){
 	console.log("You hit the root");
-	res.sendFile(path.join(__dirname, "../../../index.html"));
+	res.sendFile(path.join(__dirname, "../../index.html"));
+	console.log(path.join(__dirname, "../../index.html"));
 
 });
 app.get("/view", function(req,res){
-	res.sendFile(path.join(__dirname, "../../../view.html"));
+	res.sendFile(path.join(__dirname, "../../view.html"));
 });
 app.get("/reserve", function(req,res){
-	res.sendFile(path.join(__dirname, "../../../reserve.html"));
+	res.sendFile(path.join(__dirname, "../../reserve.html"));
 });
 //accept api requests and return relevant json
 	//if POST api request, push new reservation to array
-	app.post("/api/tables", function(req,res){
+	app.post("/api/:reservation?", function(req,res){
+		var reservation = req.params.reservation;
+		if(reservation==="tables"){
+			//get json info for everyone who has a table
+			return req.json(tables);
+		}
+
+		else if(reservation==="waitlist"){
+			//get json for waitlist
+			return req.json(waitlist);
+		}
+
 		var newReservation = req.body;
 		return res.status(201).json(newReservation);
 		
